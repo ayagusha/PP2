61b2e08511db53abd48e07875fcd406ae1760bca
ayagusha <iskakova.ayazhan@mail.ru>
2023-02-26 15:52:08 +0600

    upd

--- tsis2/lab4/json/1.py
+++ tsis2/lab4/json/1.py
@@ -1,24 +1,12 @@
 import json
+with open("sample_data.json", "r") as file:
+    data = json.load(file)
 
-y = open ("sample_data.json",'r')
-json_object = json.load(y)
-
-print("""Interface Status
-================================================================================
-DN                                                 Description           Speed    MTU  
--------------------------------------------------- --------------------  ------  ------""")
-
-for x in json_object["imdata"]:
-    for y in x["l1PhysIf"]:
-        for f in x["l1PhysIf"][y]:
-            if f == 'dn':
-                DN = x["l1PhysIf"][y][f]
-            if f == "speed":
-                Speed = x["l1PhysIf"][y][f]
-            if f == "mtu":
-                MTU = x["l1PhysIf"][y][f]
-            if f == "description":
-                Description =  x["l1PhysIf"][y][f]
-            else: 
-                Description = ""
-    print(f"{DN}                            {Description}  {Speed}   {MTU}")
+print("Interface status")
+print("=" * 80)
+print("DN", " " * 40, "Description ", "speed", " " * 10, "MTU")
+print("-" * 41, "-" * 12, "-" * 13, "\t", "-" * 4)
+for imdata in data["imdata"]:
+    for i in imdata:
+        for j in imdata[i]:
+            print(imdata[i][j]["dn"],"\t", "\t"  , imdata[i][j]["speed"] ,"\t" , imdata[i][j]["mtu"])
